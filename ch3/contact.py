41864423dc309fbb1d42e4fed4d0e8932728b073
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-18 10:25:00 +0200

    create class Supplier that inheret from Contact with Method order that display the order and supplier name

--- ch3/contact.py
+++ ch3/contact.py
@@ -5,3 +5,7 @@ class Contact:
         self.name = name
         self.email = email
         Contact.all_contacts.append(self)
+class Supplier(Contact):
+    def order(self,order):
+        print("If This were a real system we will send"
+        "'{}' order to '{}'".format(order,self.name))
