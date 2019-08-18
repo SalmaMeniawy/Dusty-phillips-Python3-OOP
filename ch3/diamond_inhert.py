581b2beb902e67942eedc8c2208dfeb892a17ab0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-18 13:50:29 +0200

    create leftClass that inherit from BaseClass which have property num_left_calls and method call_me which override the parent method

--- ch3/diamond_inhert.py
+++ ch3/diamond_inhert.py
@@ -1,6 +1,15 @@
 class BaseClass:
     num_base_calls = 0
+
     def call_me(self):
         print ("Calling method in base Class")
         self.num_base_calls += 1
 
+
+class LeftClass(BaseClass):
+    num_left_calls = 0
+
+    def call_me(self):
+        BaseClass.call_me(self)
+        print ("Print calling method in left class")
+        self.num_left_calls += 1
