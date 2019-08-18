e85a775b846a1290570f9ba344e152f612a7aedd
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-18 16:10:16 +0200

    create class MP3File that inhiret from AudioFile class and have property ext that describe the extention , method called play that display and print the filename

--- ch3/Polymorphism .py
+++ ch3/Polymorphism .py
@@ -3,3 +3,8 @@ class AudioFile:
         if not filename.endswith(self.ext):
             raise Exception("Invalid file formate")
 
+class MP3File(AudioFile):
+    ext = "mp3"
+    def play(self):
+        print ("playing {} as mp3 ".format(self.filename))
+    
