fc45bc0567f2a1a9941bc51986b07a02e34634b4
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-20 18:29:48 +0200

    create prompt_init method which return dictionary that taken from screen

--- case_study_3/estate_application.py
+++ case_study_3/estate_application.py
@@ -1,6 +1,6 @@
 class property:
-    def __init__(self,baths='',beds='',square_feet='',**kwargs):
-        super(property,self).__init__(**kwargs)
+    def __init__(self, baths='', beds='', square_feet='', **kwargs):
+        super(property, self).__init__(**kwargs)
         self.square_feet = square_feet
         self.num_bedrooms = beds
         self.num_baths = baths
@@ -13,5 +13,12 @@ class property:
         print ("bedrooms : {}").format(self.num_bedrooms)
         print ("bathrooms : {}").format(self.num_baths)
         print ()
-        
 
+    def prompt_init(self):
+        return dict(
+            square_feet=input("Enter the square feet"),
+            beds=input("Enter Number of Bedrooms"),
+            baths=input("Enter Number of baths"),
+        )
+
+    prompt_init = staticmethod(prompt_init)
