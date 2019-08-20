1ee438bfba24a6e1930b5323a053d44ae2e82c47
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-20 14:36:52 +0200

    create method __subclasshook__ with decorator classmethod

--- ch3/create_abstract_class.py
+++ ch3/create_abstract_class.py
@@ -6,5 +6,14 @@ class MediaLoader(metaclass=abc.ABCMeta):
     @abc.abstractproperty
     def ext(self):
         pass
-    
+
+    @classmethod
+    def __subclasshook__(self,cls,c):
+        if cls in MediaLoader:
+            attrs = set(dir(c))
+            if set(cls.__abstractmethod__) <= attrs:
+                return True
+        return NotImplemented
+
+
         
