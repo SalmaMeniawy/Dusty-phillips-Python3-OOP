9bb32001af5ba635d08e8638ccc141bb9c187b2e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-23 14:37:08 +0200

    create class ApartmentPurchase that have method prompt_init that have decorator that contain call prompt in it is parents

--- case_study_3/estate_application.py
+++ case_study_3/estate_application.py
@@ -179,3 +179,11 @@ class ApartmentRental(Apartment ,Rental):
         init = Apartment.prompt_init()
         init.update(Rental.prompt_init())
         return init
+
+
+class ApartmentPurchase(Purchase , Apartment):
+    @staticmethod
+    def prompt_init():
+        init = Apartment.prompt_init()
+        init.update(Rental.prompt_init())
+        return  init
