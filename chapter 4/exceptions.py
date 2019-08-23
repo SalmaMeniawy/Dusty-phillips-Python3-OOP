a7593318d5ff3f4da4b2c60d8eb019585ece97c5
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-23 20:55:47 +0200

    create a no_return function to test the exception in python

--- chapter 4/exceptions.py
+++ chapter 4/exceptions.py
@@ -3,6 +3,11 @@
     to a list only if they are even numbered integers:
 
 """
+def no_return():
+    print ("I am about to rain exception ")
+    raise Exception("this is always raised")
+    print ("this line will not execute")
+    return  "I won't be returned"
 class EvenOnly(list):
     def append(self,integer):
         if not isinstance(integer,int):
