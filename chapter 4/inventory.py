f2c07404d092f916960a4311ce3caa62d2dfb67e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-24 18:06:18 +0200

    create class Inventory that contain method luck but it is empty method contain pass

---
+++ chapter 4/inventory.py
@@ -0,0 +1,11 @@
+class Inventory:
+    def luck(self, item_type):
+        '''Select the type of item that is going to
+        be manipulated. This method will lock the
+        item so nobody else can manipulate the
+        inventory until it's returned. This prevents
+        selling the same item to two different
+        customers.'''
+        pass
+
+    
