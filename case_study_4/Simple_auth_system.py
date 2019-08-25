89b7272b753b99dfe8492cf575381c52305892a3
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-08-25 12:15:35 +0200

    create method check_permission that take perm_name and username

--- case_study_4/Simple_auth_system.py
+++ case_study_4/Simple_auth_system.py
@@ -60,6 +60,9 @@ class InvalidUsername(AuthException):
 
 class InvalidPassword(AuthException):
     pass
+class PermissionError(AuthException):
+    pass
+
 
 authenticator = Authenticator()
 
@@ -87,4 +90,19 @@ class Authorizer:
             if username not in self.authenticator.users:
                 raise InvalidUsername (username)
             perm_set.add(username)
+
+    def check_permission(self,perm_name , username):
+        if not self.authenticator.is_logged_in[username]:
+            raise NotLoggedInError(username)
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError :
+            raise PermissionError("Permission does not exist ")
+        else:
+            if username not in perm_set:
+                raise NotPermittedError (username)
+            else:
+                return True
+
+
 
